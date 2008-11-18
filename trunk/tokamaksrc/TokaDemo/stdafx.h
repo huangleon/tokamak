1c361dcbd656cd661c89f84877ecd914f08b3979
Huang Liang <liang.huang@transmension.com>
2008-11-18 09:16:36 +0000

    Comment the code that cannot be used under linux.
    git-svn-id: http://192.168.1.11/svn/tokamak@9 6d9c8ab7-4ea8-478c-ae9a-f8388889f3df

--- trunk/tokamaksrc/TokaDemo/stdafx.h
+++ trunk/tokamaksrc/TokaDemo/stdafx.h
@@ -8,8 +8,6 @@
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #include <stdio.h>
-#include <tchar.h>
-
-
+//#include <tchar.h>
 
 // TODO: reference additional headers your program requires here
